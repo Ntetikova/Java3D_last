f7cf7e63bd90f67bb66a6bbde12efb5d54b74de9
Тетикова Наталья <ntetikva@gmail.com>
2023-02-01 14:08:30 +0300

    Changed code

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,28 +1,10 @@
 public class CreditPaymentService {
-    public double calculate(double pc, double period, double sum) {
-        double pc1 = pc / period / 100;
-        double pc2 = pc1 * Math.pow(1 + pc1, period);
-        double pc3 = pc2 / ((Math.pow((1 + pc1), period)) - 1);
-        double pv = pc3 * sum;
+    public double calculate(double percentageRate, double creditPeriod, double creditSum) {
+        double pc1 = percentageRate / creditPeriod / 100;
+        double pc2 = pc1 * Math.pow(1 + pc1, creditPeriod);
+        double pc3 = pc2 / ((Math.pow((1 + pc1), creditPeriod)) - 1);
+        double initialPayment = pc3 * creditSum;
 
-        return pv;
-    }
-
-    public double calculate_a(double a_pc, double a_period, double a_sum) {
-        double a_pc1 = a_pc / a_period / 100;
-        double a_pc2 = a_pc1 * Math.pow(1 + a_pc1, a_period);
-        double a_pc3 = a_pc2 / ((Math.pow((1 + a_pc1), a_period)) - 1);
-        double a_pv = a_pc3 * a_sum;
-
-        return a_pv;
-    }
-
-    public double calculate_b(double b_pc, double b_period, double b_sum) {
-        double b_pc1 = b_pc / b_period / 100;
-        double b_pc2 = b_pc1 * Math.pow(1 + b_pc1, b_period);
-        double b_pc3 = b_pc2 / ((Math.pow((1 + b_pc1), b_period)) - 1);
-        double b_pv = b_pc3 * b_sum;
-
-        return b_pv;
+        return initialPayment;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        double pc = 9.99; // процентная ставка
-        double period = 12; // период
-        double sum = 1_000_000; //сумма кредита
-        double pv = service.calculate(pc, period, sum);
-        System.out.println("На сроке 12 месяцев ежемесячный платёж составит: " + pv + " руб");
+        /*
+        percentageRate - процентная ставка
+        creditPeriod - срок кредита
+        creditSum - сумма кредита
+         */
+        System.out.println();
+        double monthlyPaymentForOneYear = service.calculate(9.99, 12, 1_000_000);
+        System.out.println("На сроке 12 месяцев ежемесячный платёж составит: " + monthlyPaymentForOneYear + " руб");
 
         System.out.println();
-        double a_pc = 9.99; // процентная ставка
-        double a_period = 24; // период
-        double a_sum = 1_000_000; //сумма кредита
-        double a_pv = service.calculate_a(a_pc, a_period, a_sum);
-        System.out.println("На сроке 24 месяца ежемесячный платёж составит: " + a_pv + " руб");
+        double monthlyPaymentForTwoYears = service.calculate(9.99, 24, 1_000_000);
+        System.out.println("На сроке 24 месяца ежемесячный платёж составит: " + monthlyPaymentForTwoYears + " руб");
 
         System.out.println();
-        double b_pc = 9.99; // процентная ставка
-        double b_period = 36; // период
-        double b_sum = 1_000_000; //сумма кредита
-        double b_pv = service.calculate_b(b_pc, b_period, b_sum);
-        System.out.println("На сроке 36 месяцев ежемесячный платёж составит: " + b_pv + " руб");
+        double monthlyPaymentForThreeYears = service.calculate(9.99, 36, 1_000_000);
+        System.out.println("На сроке 36 месяцев ежемесячный платёж составит: " + monthlyPaymentForThreeYears + " руб");
+
     }
 }
