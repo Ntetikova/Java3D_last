151f72e76848050668999758d697cb98189f9dd0
Тетикова Наталья <ntetikva@gmail.com>
2023-02-06 15:07:28 +0300

    fixed code

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,6 +1,6 @@
 public class CreditPaymentService {
     public double calculate(double percentageRate, double creditPeriod, double creditSum) {
-        double pc1 = percentageRate / creditPeriod / 100;
+        double pc1 = percentageRate / 100 / 12;
         double pc2 = pc1 * Math.pow(1 + pc1, creditPeriod);
         double pc3 = pc2 / ((Math.pow((1 + pc1), creditPeriod)) - 1);
         double initialPayment = pc3 * creditSum;
